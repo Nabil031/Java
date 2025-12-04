6dd58d73e86d34325f16b0572960a0ed92a4bb77
Nabil <302472936@student.rocmondriaan.nl>
2025-12-04 15:19:48 +0100

    les5

---
+++ opdracht5p2/script.js
@@ -0,0 +1,41 @@
+const plyer = {
+    name: "Max",
+    Symbol: "x",
+    score: 0,
+    addpoint () {
+    this.score =+ 1;
+}
+}
+
+console.log (plyer.score)
+plyer.addpoint();
+
+console. log (plyer.score)
+
+
+
+const game = {
+
+    currplyer: "x",
+    isOver:false
+};
+
+console.log(game);
+
+const highscores = [​
+  { name: 'Alice', score: 10 },​
+  { name: 'Bob', score: 15 },​
+  { name: 'Charlie', score: 8 }​
+];​
+
+highscores.push( {name: "Mini", scire:20})
+
+console.log(highscores[1].name); 
+
+console.log(highscores[0].score); 
+for(let i = 0; i < highscores.length;i++)  {
+    console.log(naam: ${highscores[i].name}, a
+    
+
+
+}
